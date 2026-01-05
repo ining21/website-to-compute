673f2001b450d605147b214fb429ccc28491e124
ining21 <89717154+ining21@users.noreply.github.com>
2026-01-05 12:06:54 +0800

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'groovity-cache-v1';
+const ASSETS = [
+  '.',
+  './index.html',
+  './manifest.json'
+  // add icon paths if you add icons
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
